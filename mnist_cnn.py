e66132149620c5811bed32d649a79c0d71617196
palloc <palloc.mknet10@gmail.com>
2017-04-21 00:27:39 +0900

    separate

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -2,57 +2,16 @@
 
 from __future__ import print_function
 import keras
-from keras.datasets import mnist
-from keras.models import Sequential
+import tensorflow as tf
+from keras import backend as K
+from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
-from keras import backend as K
-import tensorflow as tf
-from keras.models import Model
 from keras.layers import Input
 from keras.layers.core import Lambda
 from keras.layers.merge import Concatenate
-from keras import backend as K
-from keras.models import Model
-from keras.layers import Input
-from keras.layers.core import Lambda
-from keras.layers.merge import Concatenate
-
-def slice_batch(x, n_gpus, part):
-        """
-        Divide the input batch into [n_gpus] slices, and obtain slice no. [part].
-        i.e. if len(x)=10, then slice_batch(x, 2, 1) will return x[5:].
-        """
-        sh = K.shape(x)
-        L = sh[0] / n_gpus
-        if part == n_gpus - 1:
-            return x[part*L:]
-        return x[part*L:(part+1)*L]
-            
-        
-def to_multi_gpu(model, n_gpus=2):
-        """Given a keras [model], return an equivalent model which parallelizes
-        the computation over [n_gpus] GPUs.
-        
-        Each GPU gets a slice of the input batch, applies the model on that slice
-        and later the outputs of the models are concatenated to a single tensor,
-        hence the user sees a model that behaves the same as the original.
-        """
-        with tf.device('/cpu:0'):
-                x = Input(model.input_shape[1:], name="a")#model.input_names[0])
-        
-        towers = []
-        for g in range(n_gpus):
-                with tf.device('/gpu:' + str(g)):
-                        slice_g = Lambda(slice_batch, lambda shape: shape, arguments={'n_gpus':n_gpus, 'part':g})(x)
-                        towers.append(model(slice_g))
-                
-        with tf.device('/cpu:0'):
-                merged = Concatenate(axis=0)(towers)
-            
-        return Model(inputs=[x], outputs=[merged])
-        
+from keras.datasets import mnist
+from .MultiGPU import ToMultiGPU
 
 
 class MnistClassification:
@@ -61,15 +20,16 @@ class MnistClassification:
         self.batch_size = 128
         self.num_classes = 10
         self.epochs = 20
+        # Image size
         self.img_rows = 28
         self.img_cols = 28
         
-
+        
     def model_definition(self):
         self.model = Sequential()
         self.model.add(Conv2D(32, kernel_size=(3, 3),
-                         activation='relu',
-                         input_shape=self.input_shape))
+                              activation='relu',
+                              input_shape=self.input_shape))
         self.model.add(Conv2D(64, (3, 3), activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         self.model.add(Dropout(0.25))
@@ -78,20 +38,11 @@ class MnistClassification:
         self.model.add(Dropout(0.5))
         self.model.add(Dense(self.num_classes, activation='softmax'))
 
-        
-        self.model = to_multi_gpu(self.model)
+        self.model = ToMultiGPU.to_multi_gpu(self.model)
 
         self.model.compile(loss=keras.losses.categorical_crossentropy,
-                      optimizer=keras.optimizers.Adadelta(),
-                      metrics=['accuracy'])
-
-
-        self.model.fit(self.x_train, self.y_train,
-                  batch_size=self.batch_size,
-                  epochs=self.epochs,
-                  verbose=1,
-                  validation_data=(self.x_test, self.y_test))
-
+                           optimizer=keras.optimizers.Adadelta(),
+                           metrics=['accuracy'])
 
 
     def read_dataset(self):
@@ -118,10 +69,16 @@ class MnistClassification:
         self.y_test = keras.utils.to_categorical(y_test, self.num_classes)
         
 
-
     def __call__(self):
         self.read_dataset()
         self.model_definition()
+
+        # Run
+        self.model.fit(self.x_train, self.y_train,
+                       batch_size=self.batch_size,
+                       epochs=self.epochs,
+                       verbose=1,
+                       validation_data=(self.x_test, self.y_test))
 
         self.score = self.model.evaluate(self.x_test, self.y_test, verbose=0)
 
@@ -129,7 +86,6 @@ class MnistClassification:
         print('Test accuracy:', self.score[1])
     
 
-    
 if __name__ == '__main__':
     test = MnistClassification()
     test()
